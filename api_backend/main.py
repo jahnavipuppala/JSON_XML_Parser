36a28310cbde47c0cda8390518c8d3d981cec321
Saachi Kaup <saachi.kaup@gmail.com>
2024-04-06 20:55:14 +0530

    Code changes for nesting

--- api_backend/main.py
+++ api_backend/main.py
@@ -12,7 +12,7 @@ app = FastAPI()
 URL_CLIENT = "mongodb://localhost:27017/"
 CLIENT = MongoClient(URL_CLIENT,  server_api=ServerApi('1'))
 
-DB_NAME = "dmi_testing"
+DB_NAME = "MAIN_DMI_TESTING"
 DB = CLIENT[DB_NAME]
 
 def insert_one_in_mongo_db(parsed_data, collection_name, db = DB):
@@ -29,7 +29,11 @@ def copy_values_if_dict(values):
         return copy
     return values
 
-def add_parent_id(values, parent_random_id):
+def add_parent_id(values, parent_random_id, key):
+    if not isinstance(values, dict):
+        dictionary = {}
+        dictionary[key] = values
+        return dictionary
     values["parent_random_id"] = parent_random_id
     return values
 
@@ -37,12 +41,17 @@ def recursive_configuration(parsed_data, metadata_tables, base_table):
     to_insert = {}
     random_id = random.random()
     to_insert["random_id"] = random_id
+    print(metadata_tables)
     for key, values in parsed_data.items():
+        print(key)
+        if isinstance(values, dict):
+            recursive_configuration(add_parent_id(values, random_id, key), metadata_tables, base_table)
         if key in metadata_tables["ONE_TO_ONE"]:
-            recursive_configuration(add_parent_id(values, random_id), metadata_tables, key)
+            print("KEY", key)
+            recursive_configuration(add_parent_id(values, random_id, key), metadata_tables, key)
         elif key in metadata_tables["ONE_TO_MANY"]:
             for child in values:
-                recursive_configuration(add_parent_id(child, random_id), metadata_tables, key)
+                recursive_configuration(add_parent_id(child, random_id, key), metadata_tables, key)
         else:
             if key in metadata_tables["MANDATORY"]:
                 raise KeyError("Mandatory Key Not present in metadata")
@@ -57,6 +66,7 @@ async def parse_json(json_data: str):
 
 @app.post("/parse_json_and_store_in_mongo_db")
 async def parse_json(json_data: str, metadata_tables: dict):
+    print("In api")
     parsed_json = json.loads(json_data)
     recursive_configuration(parsed_json, metadata_tables, base_table = BASE_TABLE)
     return parsed_json 
